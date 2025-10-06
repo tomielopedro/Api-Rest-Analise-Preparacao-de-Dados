2dfa49a7e461db82b1735076243c25ac25be619a
Pedro Tomielo <tomielopedro586@gmail.com>
2025-10-06 10:21:26 -0300

    add: get methods

--- Api.py
+++ Api.py
@@ -3,15 +3,36 @@ import pandas as pd
 from dataclasses import dataclass, asdict
 from flasgger import Swagger
 import json
+from dataclasses import dataclass
 
 
 @dataclass
-class Pessoa:
-    id:int
-    link: str
+class Ator:
     nome: str
-    eh_pessoa: bool
-    probabilidade: float
+    papel: str
+    n_ep: int
+
+    def to_dict(self):
+        return {
+            "nome": self.nome,
+            "personagem": self.papel,
+            "quantidade_episodios": self.n_ep
+        }
+
+
+@dataclass
+class Serie:
+    nome: str
+    ordem: int
+    ano_estreia: int
+    ano_encerramento: int
+    episodios: int
+    faixa_etaria: int  # Pode ser número ou "Livre" == 0
+    avaliacao: float
+    link: str
+    popularidade: float
+    atores: list
+
 
     def to_json(self) -> str:
         return json.dumps(asdict(self), ensure_ascii=False)
@@ -20,61 +41,92 @@ app = Flask(__name__)
 swagger = Swagger(app)
 
 
-df = pd.read_csv('data/resultado_links.csv')
+df = pd.read_csv('data/series.csv')
 lista_links = [
-    Pessoa(
-        id=row["id"],
-        link=row["link"],
-        nome=row["nome"],
-        eh_pessoa=bool(row["eh_pessoa"]),
-        probabilidade=row["probabilidade"],
+    Serie(
+        nome=row['titulo'],
+        ordem=row['ordem'],
+        ano_estreia=row['ano_estreia'],
+        ano_encerramento=row['ano_encerramento'],
+        episodios=row['episodios'],
+        faixa_etaria=row['classificacao_indicativa'],
+        avaliacao=row['nota_imdb'],
+        link=row['link'],
+        popularidade=row['popularidade'],
+        atores=[]
+
     )
     for row in df.to_dict(orient="records")
 ]
 
-@app.route("/wikipedia_links/", defaults={"id": None}, methods=["GET"])
-@app.route("/wikipedia_links/<int:id>/", methods=["GET"])
-def wikipedia_links(id):
-    """
-    Retorna informações sobre pessoas cadastradas no sistema.
 
+@app.route("/series/")
+def series():
+    """
+    Lista as séries disponíveis
     ---
+    tags:
+      - Séries
+    description: Retorna uma lista das 200 primeiras séries cadastradas.
+    responses:
+      200:
+        description: Lista de séries retornada com sucesso
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              ordem:
+                type: integer
+                example: 1
+              nome:
+                type: string
+                example: "Pessoa 1"
+    """
+    return jsonify([asdict(p) for p in lista_links[0:200]]), 200
+
+
+@app.route("/series/<int:id>/", methods=["GET"])
+def series_by_id(id):
+    """
+    Retorna uma série específica pelo ID
+    ---
+    tags:
+      - Séries
     parameters:
       - name: id
         in: path
         type: integer
-        required: false
-        description: ID da pessoa (se não informado, retorna 200 pessoas)
+        required: true
+        description: ID da série
     responses:
       200:
-        description: Retorna a(s) pessoa(s) encontrada(s)
+        description: Série encontrada com sucesso
         schema:
           type: object
           properties:
-            id:
+            ordem:
               type: integer
+              example: 1
             nome:
               type: string
-            link:
-              type: string
-            eh_pessoa:
-              type: boolean
-            probabilidade:
-              type: number
+              example: "Pessoa 1"
       404:
-        description: Pessoa não encontrada
+        description: Série não encontrada
+        schema:
+          type: object
+          properties:
+            erro:
+              type: string
+              example: "Pessoa não encontrada"
     """
-    # Se nenhum ID for fornecido, retorna a lista completa
-    if id is None:
-        return jsonify([asdict(p) for p in lista_links[0:200]]), 200
-
-    # Busca pessoa pelo ID
-    pessoa_encontrada = next((p for p in lista_links if p.id == id), None)
+    pessoa_encontrada = next((p for p in lista_links if p.ordem == id), None)
 
     if pessoa_encontrada:
         return jsonify(asdict(pessoa_encontrada)), 200
     else:
         return make_response(jsonify({"erro": "Pessoa não encontrada"}), 404)
+
 
 
 @app.route("/wikipedia_test", methods=["GET"])
